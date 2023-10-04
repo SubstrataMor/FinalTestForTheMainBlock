a043a7f4bf512c19308d48ffb2129fb7a8abcfca
Substrata <digital_strata@mail.ru>
2023-10-05 02:07:22 +0300

    Added Readme file and solution for Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = {"Hello", "2", "world", ":-)"};
+string[] arr2 = {"1234", "1567", "-2", "computer science"};
+string[] arr3 = {"Russia", "Denmark", "Kazan"};
+void ResultArray(string[] array)
+{
+    var resultArr = new string[array.Length];
+    var element = 0;
+    foreach (var value in array)
+    {
+        if (value.Length <= 3)
+        {
+            resultArr[element] = value;
+            element++;
+        }
+    }
+    PrintArray(resultArr);
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    foreach (var item in array )
+    {
+        if (item != null)
+            Console.Write($"{item},");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+ResultArray(arr1);
+ResultArray(arr2);
+ResultArray(arr3);
